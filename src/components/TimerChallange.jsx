9e799017f73386dfdef1926850b86cd49f63892c
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-18 19:12:29 +0000

    Using refs for clearing timer

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 export default function TimerChallange({title,targetTime}) {
+
+  const timer = useRef()
+  const[timerExpired,setTimerExpired]=useState(false)
+  const[timerStarted,setTimerStarted]=useState(false)
+
+  function handleTimerStart(){
+    timer.current = setTimeout(()=>{
+        setTimerExpired(true)
+    },targetTime*1000)
+    setTimerStarted(true)
+  }
+
+  function handleTimerStop(){
+    clearTimeout(timer.current)
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired&&<p>You Lost</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime>1&&'s'}
       </p>
       <p>
-        <button>Stop challange/start</button>
+        <button onClick={timerStarted ? handleTimerStop : handleTimerStart}>{timerStarted?"Stop":'Start'} Challange</button>
       </p>
-      <p className=''>
-        Timer is running/inactive
+      <p className={timerStarted?'active':undefined}>
+        {timerStarted?"Timer is running...":"Timer inactive"}
       </p>
     </section>
   )
